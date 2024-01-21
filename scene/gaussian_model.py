efd0b8688ff6ec5e25ae893d287c663f703fea40
Heng <31088159+shiukaheng@users.noreply.github.com>
2024-01-21 08:39:50 +0000

    works! deleted nn.Parameter

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -90,12 +90,12 @@ class GaussianModel:
 
         opacities = inverse_sigmoid(0.1 * torch.ones((fused_point_cloud.shape[0], 1), dtype=torch.float, device="cuda"))
 
-        self._xyz = nn.Parameter(fused_point_cloud.requires_grad_(True))
-        self._features_dc = nn.Parameter(features[:,:,0:1].transpose(1, 2).contiguous().requires_grad_(True))
-        self._features_rest = nn.Parameter(features[:,:,1:].transpose(1, 2).contiguous().requires_grad_(True))
-        self._scaling = nn.Parameter(scales.requires_grad_(True))
-        self._rotation = nn.Parameter(rots.requires_grad_(True))
-        self._opacity = nn.Parameter(opacities.requires_grad_(True))
+        self._xyz = fused_point_cloud
+        self._features_dc = features[:,:,0:1].transpose(1, 2).contiguous()
+        self._features_rest = features[:,:,1:].transpose(1, 2).contiguous()
+        self._scaling = scales
+        self._rotation = rots
+        self._opacity = opacities
         self.max_radii2D = torch.zeros((self.get_xyz.shape[0]), device="cuda")
 
     def training_setup(self, training_args):
